761eef6f600294783d7e0dbe0ab650a12df39714
Djielo <business.jlo68@gmail.com>
2022-09-14 12:03:40 +0200

    feat: First approach to storing employeeList

---
+++ src/Utils/employeesList.js
@@ -0,0 +1,16 @@
+export const employeesList = JSON.parse(localStorage.getItem("employees"));
+
+"#employee-table".DataTable({
+  data: employeesList,
+  columns: [
+    { title: "First Name", data: "firstName" },
+    { title: "Last Name", data: "lastName" },
+    { title: "Start Date", data: "startDate" },
+    { title: "Department", data: "department" },
+    { title: "Date of Birth", data: "dateOfBirth" },
+    { title: "Street", data: "street" },
+    { title: "City", data: "city" },
+    { title: "State", data: "state" },
+    { title: "Zip Code", data: "zipCode" },
+  ],
+});
