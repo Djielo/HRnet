41ac278815adfbeebaa84333502a182bd3a37d41
Djielo <business.jlo68@gmail.com>
2022-10-06 21:14:55 +0200

    feat: Organize Form on Home page with DatePicker and Select components

--- React-version/src/components/Formulary/Formulary.jsx
+++ React-version/src/components/Formulary/Formulary.jsx
@@ -1,13 +1,14 @@
 import React, { useRef } from "react";
-import { useEffect } from "react";
 import { infoNewEmployee } from "../../Utils/infoNewEmployee";
-import { stateSelectOptions } from "../../Utils/stateSelectOptions";
 import { useDispatch } from "react-redux";
+import MyDatePicker from "../MyDatePicker/MyDatePicker";
+import Select from "react-select";
+import states from "../../Utils/states";
+import departments from "../../Utils/departments";
 
 const Formulary = () => {
   const formRef = useRef();
   const dispatch = useDispatch();
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const message = defineMessage();
@@ -16,15 +17,16 @@ const Formulary = () => {
   };
 
   function defineMessage() {
+    console.log(formRef.current[7])
     if (isFilled(formRef.current[0].value)) return "you must fill your first name";
     if (isFilled(formRef.current[1].value)) return "you must fill your last name";
     if (isFilled(formRef.current[2].value)) return "you must select your Date of Birth";
     if (isFilled(formRef.current[3].value)) return "you must select the date you be employed";
     if (isFilled(formRef.current[5].value)) return "you must fill your Street Address";
     if (isFilled(formRef.current[6].value)) return "you must fill your City";
-    if (isFilled(formRef.current[7].value)) return "you must select your State";
+    if (isFilled(document.querySelector(".css-qc6sy-singleValue").innerText)) return "you must select your State";
     if (isFilled(formRef.current[8].value)) return "you must fill your Zip Code";
-    if (isFilled(formRef.current[9].value)) return "you must select your workin department";
+    if (isFilled(document.querySelector(".css-6j8wv5-Input"))) return "you must select your working department";
     return `welcome ${formRef.current[0].value}, you are registred as new employee :)`;
   }
 
@@ -32,44 +34,34 @@ const Formulary = () => {
     return value === undefined || value === "";
   }
 
-  useEffect(() => {
-    stateSelectOptions();
-  });
-
   return (
     <form ref={formRef} className="container-form" onSubmit={(e) => handleSubmit(e)}>
       <h2 className="form-h2">Create Employee</h2>
       <div className="form-group1-2">
         <div className="form-group1">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <input type="text" id="first-name" placeholder="First Name" />
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <input type="text" id="last-name" placeholder="Last Name" />
           <label htmlFor="date-of-birth">Date of Birth</label>
-          <input type="date" id="date-of-birth" />
+          <MyDatePicker id="date-of-birth" placeholder="Birthdate" />
           <label htmlFor="start-date">Start Date</label>
-          <input type="date" id="start-date" />
+          <MyDatePicker id="start-date" />
         </div>
         <fieldset className="form-group2">
           <legend>Address</legend>
           <label htmlFor="street">Street</label>
-          <input type="text" id="street" />
+          <input type="text" id="street" placeholder="Street" />
           <label htmlFor="city">City</label>
-          <input type="text" id="city" />
+          <input type="text" id="city" placeholder="City" />
           <label htmlFor="state">State</label>
-          <select id="state" />
+          <Select classNamePrefix="react-select" id="state" options={states} placeholder="State" />         
           <label htmlFor="zip-code">Zip Code</label>
-          <input type="text" id="zip-code" />
+          <input type="text" id="zip-code" placeholder="Zip Code" />
         </fieldset>
       </div>
       <label htmlFor="department">Department</label>
-      <select id="department">
-        <option value="sales">Sales</option>
-        <option value="marketing">Marketing</option>
-        <option value="engineering">Engineering</option>
-        <option value="human-resources">Human Resources</option>
-        <option value="legal">Legal</option>
-      </select>
+      <Select classNamePrefix="react-select" id="department" options={departments} placeholder="Department" />
       <button className="modal-btn">Save</button>
     </form>
   );
