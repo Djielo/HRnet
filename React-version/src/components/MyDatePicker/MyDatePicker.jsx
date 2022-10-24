5ea52a274aa602ca243578eef952161fad7bec0b
Djielo <business.jlo68@gmail.com>
2022-10-24 10:52:41 +0200

    feat: Comments in MyDatePicker.jsx

--- React-version/src/components/MyDatePicker/MyDatePicker.jsx
+++ React-version/src/components/MyDatePicker/MyDatePicker.jsx
@@ -3,6 +3,13 @@ import DatePicker from "react-datepicker";
 import { useDispatch, useSelector } from "react-redux";
 import { setBirthDate, setStartDate } from "../../redux/features/employeeSlice";
 
+/**
+ * A React component that is a wrapper for the DatePicker component. It is a functional component.
+ * 
+ * @param {string} id - The id of the input element.
+ * @param {string} placeholder - The placeholder of the input element.
+ * @returns {JSX.Element}
+ */
 const MyDatePicker = ({id, placeholder}) => {
   const dispatch = useDispatch();
   const selectBirthdate = useSelector((state) => state.employee.birthDate);
@@ -12,7 +19,11 @@ const MyDatePicker = ({id, placeholder}) => {
     <DatePicker
       id={id}
       placeholderText={placeholder}
+      /* A ternary operator to know which input is selected. */
       selected={id === "date-of-birth" ? selectBirthdate : selectStartdate}
+      /* A ternary operator that checks if the id is "date-of-birth" and if it is, it will dispatch the
+      setBirthDate action with the date.getTime() as the payload. Otherwise, it
+      will dispatch the setStartDate action with the same conditions. */
       onChange={(date) => dispatch(id === "date-of-birth" ? setBirthDate(date.getTime()) : setStartDate(date.getTime()))}
       showYearDropdown
       dateFormatCalendar="MMMM"
