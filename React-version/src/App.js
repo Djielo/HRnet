e5fb13933aba836b3facdd80e47d4332bb0fb5d4
Djielo <business.jlo68@gmail.com>
2022-10-26 15:37:53 +0200

    feat: Added lazy to improve code performance

--- React-version/src/App.js
+++ React-version/src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Employee from "./pages/Employee";
-import Error from "./pages/Error";
 import { Provider } from "react-redux";
 import { store } from "./redux/app/store";
+import React, { Suspense } from "react";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Employee = React.lazy(() => import("./pages/Employee"));
+const Error = React.lazy(() => import("./pages/Error"));
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/employee" element={<Employee />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/employee" element={<Employee />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </Provider>
   );
