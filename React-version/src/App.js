c809571c6fa5b5ae75d48a3fcb4ead4a46a809ff
Djielo <business.jlo68@gmail.com>
2022-10-31 12:52:19 +0100

    refactor: Install Redux-persist: To persist data in Local Storage

--- React-version/src/App.js
+++ React-version/src/App.js
@@ -2,14 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/app/store";
 import React, { Suspense } from "react";
+import { persistStore } from "redux-persist"; // npm install redux-persist
+import { PersistGate } from "redux-persist/integration/react"; // npm install redux-persist
 
 const Home = React.lazy(() => import("./pages/Home"));
 const Employee = React.lazy(() => import("./pages/Employee"));
 const Error = React.lazy(() => import("./pages/Error"));
 
+let persistor = persistStore(store); // Create a persistor from our store
+
 function App() {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}></PersistGate> {/* Add the PersistGate to our app */}
       <BrowserRouter className="App">
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
--- React-version/src/redux/app/store.js
+++ React-version/src/redux/app/store.js
@@ -1,8 +1,19 @@
-import {configureStore} from '@reduxjs/toolkit';
-import { employeeSlice } from '../features/employeeSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { employeeSlice } from "../features/employeeSlice";
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"; // npm install redux-persist
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root", // key is required
+  version: 1, // version is required
+  storage, // storage is now required
+  blacklist: ["employee"], // blacklist is now required
+};
+
+const persistedReducer = persistReducer(persistConfig, employeeSlice.reducer); // Create a new reducer with our existing reducer
 
 export const store = configureStore({
-  reducer: {
-    employee: employeeSlice.reducer,
-  },
+  reducer: { employee: persistedReducer }, // Add the new reducer to our store with any name we want
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: { ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] } }), // Add the ignoredActions to our middleware
 });
